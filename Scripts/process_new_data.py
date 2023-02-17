b0411c50e4b85482718229b9be87ed06d74a5fa8
jepietryga <jacobpietryg2026@u.northwestern.edu>
2023-02-17 17:33:25 -0600

    Merge with image segmentation

--- Scripts/process_new_data.py
+++ Scripts/process_new_data.py
@@ -2,5 +2,26 @@
 
 import sys
 sys.path.append("..")
+import pickle
+import os
+from sklearn import RandomForestRegressor 
 
 from Utility.segmentation_utils import *
+
+# Define folders
+model_folder = "../Models"
+image_folder = "../Images"
+result_folder = "../Results"
+# Load models
+model_names = ["RF_C-MC_I-P.sav","RF_C_MC.sav","RF_I_P.sav"]
+rf_CMC_IP, rf_C_MC, rf_I_P = [pickle.load(open(os.path.join(model_folder,model), 'rb'))\
+                              for model in model_names]
+
+# Grab image folder
+super_folder = glob.glob("../Images/Organized/*/")
+image_dir = super_folder[0]
+
+## Grab image
+### IS the Image
+### get df of Image
+### 
