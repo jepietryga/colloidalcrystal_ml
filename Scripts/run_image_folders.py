5972adab9a296ac0666978e677c140694cb063eb
jepietryga <jacobpietryga@gmail.com>
2024-08-09 12:27:50 -0500

    Added simple script for running single images

--- Scripts/run_image_folders.py
+++ Scripts/run_image_folders.py
@@ -1,7 +1,8 @@
-# Script will accept a an image folder that contains 
+# Script will accept a an image folder that contains
 # sub-image folders and run through them. Data will be collected
 
 import sys
+
 sys.path.append("..")
 
 from facet_ml.classification.model_using import *
@@ -22,18 +23,18 @@ from pathlib import Path
 #                threshold_mode=threshold_mode,
 #                edge_modification="localthresh",)
 
-threshold_mode = "algorithmic"
+threshold_mode = "segment_anything"
 segmenter_kwargs = {
-    "threshold_mode":"ensemble",
-    "edge_modification":"localthresh"
+    "threshold_mode": "segment_anything",
+    "edge_modification": "localthresh",
 }
 IS = ImageSegmenter(
     input_path=None,
     segmenter=threshold_mode,
-    segmenter_kwargs=segmenter_kwargs,
+    # segmenter_kwargs=segmenter_kwargs,
 )
 ## Define Images ##
-'''
+"""
 parent_image_dir = os.path.join(Path(__file__).parent.parent,"Images","2023_11_Organized_Images")
 
 parent_image_dir = os.path.join(Path(__file__).parent.parent,"Images","Fig_3_PAE_diffusion")
@@ -49,12 +50,20 @@ parent_image_dir_list.append(Path(__file__).parent.parent / "Images" / "20240217
 
 ## Define Save Path
 results_path = f"../Results/2024_02_Figure4_rate_images_{threshold_mode}"
-'''
+"""
 
 ## Target folders
 parent_image_dir_list = []
-parent_image_dir_list.append(Path(__file__).parent.parent / "Images" / "Fig_3_PAE_diffusion")
-
+# parent_image_dir_list.append(
+#     Path(__file__).parent.parent / "Images" / "Fig_3_PAE_diffusion"
+# )
+parent_image_dir_list.append(
+    Path(__file__).parent.parent
+    / "Images"
+    / "20240217_Images_by_experiment"
+    / "Fig3_Fig4_oven_pcr_linkerstrength"
+    # / "L-2_nM-2.5_mixing-F_oven-F_embed-Ag"
+)
 ## Load Features
 features = load_feature_config("default_features-agnostic")
 # features = load_feature_config("default_features")
@@ -63,61 +72,75 @@ features = load_feature_config("default_features-agnostic")
 ## Load Models
 model_set = "2024_02__original_default_features-agnostic"
 # model_set = "2024_02__original_default_features"
-model_set = "2023_original_default_features-agnostic" # Best sent RC's way
+model_set = "2023_original_default_features-agnostic"  # Best sent RC's way
 # model_set = "2024_02__original_2024_features-agnostic"
-model_folder = os.path.join(Path(__file__).parent.parent,"facet_ml","static","Models",model_set)
-model_CvMC_path = os.path.join(model_folder,"RF_C_MC.sav")
-model_CvI_path = os.path.join(model_folder,"RF_C-MC_I.sav")
-with open(model_CvMC_path,"rb") as f:
+model_folder = os.path.join(
+    Path(__file__).parent.parent, "facet_ml", "static", "Models", model_set
+)
+model_CvMC_path = os.path.join(model_folder, "RF_C_MC.sav")
+model_CvI_path = os.path.join(model_folder, "RF_C-MC_I.sav")
+with open(model_CvMC_path, "rb") as f:
     model_CvMC = pickle.load(f)
-    if isinstance(model_CvMC,dict):
+    if isinstance(model_CvMC, dict):
         model_CvMC = model_CvMC["model"]
-with open(model_CvI_path,"rb") as f:
+with open(model_CvI_path, "rb") as f:
     model_CvI = pickle.load(f)
-    if isinstance(model_CvI,dict):
+    if isinstance(model_CvI, dict):
         model_CvI = model_CvI["model"]
 
 ### MAIN BODY OF CODE ###
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 for parent_image_dir in parent_image_dir_list:
-    image_folders = [path for path in glob.glob(os.path.join(parent_image_dir,"*")) if os.path.isdir(path)]
+    image_folders = [
+        path
+        for path in glob.glob(os.path.join(parent_image_dir, "*"))
+        if os.path.isdir(path)
+    ]
+    print(parent_image_dir)
+    print(image_folders)
 
     ## Define Save Path
     save_id = Path(parent_image_dir).stem
     results_path = f"../Results/{save_id}_{threshold_mode}"
 
     for image_folder in image_folders:
-        image_list = glob.glob(os.path.join(image_folder,"*"))
-        _,image_folder_name = os.path.split(image_folder)
+        image_list = glob.glob(os.path.join(image_folder, "*"))
+        _, image_folder_name = os.path.split(image_folder)
         pbar = tqdm.tqdm(image_list)
         for image in pbar:
             image_name = Path(image).stem
             pbar.set_description(f"{image_folder_name,image_name}")
             IS.input_path = image
             IS.process_images()
-            MA_CvMC = ModelApplication(model_CvMC,IS,features=features)
-            MA_CvI = ModelApplication(model_CvI,IS,features=features,
-                                      replacement_dict={
-                                          "Crystalline":MA_CvMC,
-                                          "Not Crystalline":"Incomplete"
-                                      })
-            
+            MA_CvMC = ModelApplication(model_CvMC, IS, features=features)
+            MA_CvI = ModelApplication(
+                model_CvI,
+                IS,
+                features=features,
+                replacement_dict={
+                    "Crystalline": MA_CvMC,
+                    "Not Crystalline": "Incomplete",
+                },
+            )
+
             IS.df["Labels"] = MA_CvI.run()
-            color_img = visualize_labels(IS,IS.df)
+            color_img = visualize_labels(IS, IS.df)
 
             # Prepare to save
-            save_folder = os.path.join(results_path,image_folder_name)
-            os.makedirs(save_folder,exist_ok=True)
-            csv_path = os.path.join(save_folder,f"{image_name}_results.csv")
-            img_path = os.path.join(save_folder,f"{image_name}_visualiation.png")
+            save_folder = os.path.join(results_path, image_folder_name)
+            os.makedirs(save_folder, exist_ok=True)
+            csv_path = os.path.join(save_folder, f"{image_name}_results.csv")
+            img_path = os.path.join(save_folder, f"{image_name}_visualiation.png")
 
             # Save
-            cv2.imwrite(img_path,cv2.cvtColor(color_img,cv2.COLOR_BGR2RGB))
+            cv2.imwrite(img_path, cv2.cvtColor(color_img, cv2.COLOR_BGR2RGB))
             IS.df.to_csv(csv_path)
-        
+
         # combine all csvs
-        csv_paths = glob.glob(os.path.join(save_folder,"*.csv"))
-        df_list = list(map(pd.read_csv,csv_paths))
+        csv_paths = glob.glob(os.path.join(save_folder, "*.csv"))
+        df_list = list(map(pd.read_csv, csv_paths))
         df_total = pd.concat(df_list)
-        df_total.to_csv( os.path.join(save_folder,f"{image_folder_name}_total_results.csv"))
+        df_total.to_csv(
+            os.path.join(save_folder, f"{image_folder_name}_total_results.csv")
+        )
